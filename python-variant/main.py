6dbe61fd630c02f506befdf3dd11964d57bd0247
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2022-03-20 16:26:12 +0200

    Update main.py

--- python-variant/main.py
+++ python-variant/main.py
@@ -1,2 +1,9 @@
 # Advanced Chord Implementation in Python
 # Created by Christos-Panagiotis Balatsouras
+# ORCID: https://orcid.org/0000-0001-8914-7559
+
+# Import Python Modules
+import hashlib
+
+# Global Variables
+hash_space_bits = 4
