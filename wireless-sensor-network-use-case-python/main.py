7b7feb2f2dab5fc8b82c54004403ff3019f508dd
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2022-03-21 01:50:11 +0200

    Update main.py

--- wireless-sensor-network-use-case-python/main.py
+++ wireless-sensor-network-use-case-python/main.py
@@ -2,8 +2,17 @@
 # Created by Christos-Panagiotis Balatsouras
 # ORCID: https://orcid.org/0000-0001-8914-7559
 
+"""
+Use Case: Application of Chord Protocol Based Routing in Wireless Sensor Networks
+Application Functionality:
+1. Node Join / Leave
+2. Network Build
+3. Data Lookup Query
+"""
+
 # Import Python Modules
 import hashlib
 
 # Global Variables
 hash_space_bits = 4
+hash_space = 2**hash_space_bits
