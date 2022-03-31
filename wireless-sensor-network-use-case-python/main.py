dec3a420d79e9ab083451bc64fd9182476567f43
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2022-03-31 18:20:41 +0300

    Update main.py

--- wireless-sensor-network-use-case-python/main.py
+++ wireless-sensor-network-use-case-python/main.py
@@ -20,16 +20,49 @@ hash_space = 2 ** hash_space_bits
 
 # The class for the Sensor Network as a total entity
 class SensorNetwork:
-    pass
+    """
+    Class SensorNetwork:
+    Data: Basic Info about the Sensor Network
+    Operations:
+    1. Reload Network Details
+    """
+    def __init__(self):
+        pass
+
+    def network_reload(self):
+        pass
 
 
 # The class for each sensor node on the sensor network
 class SensorNode:
-    pass
+    """
+    Class SensorNode:
+    Data: Basic Info about each Sensor Node
+    Operations:
+    1. Join / Leave
+    2. Find successor / closest predecessor node
+    3. Node Data Lookup Query
+    4. Node Data Reload
+    """
+    def __init__(self):
+        pass
+    
+    def node_join(self):
+        pass
+    
+    def node_leave(self):
+        pass
+    
+    def lookup_query(self):
+        pass
+    
+    def data_reload(self):
+        pass
 
 
 """
 # main process
 """
 if __name__ == "__main__":  # Execute these lines, only if this module is executed by itself.
-    pass
+    print("Chord Protocol Application on Wireless Sensor Networks")
+    print("Implementation by Christos-Panagiotis Mpalatsouras")
