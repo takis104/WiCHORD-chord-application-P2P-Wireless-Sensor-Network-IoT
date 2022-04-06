2941ffff293c08d5532b31b2d3054e59c5f5292c
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2022-04-06 19:58:23 +0300

    Update main.py

--- wireless-sensor-network-use-case-python/main.py
+++ wireless-sensor-network-use-case-python/main.py
@@ -36,6 +36,7 @@ class SensorNetwork:
         self.last_node = 0  # zero value initially, to be updated later
 
     def network_reload(self):
+        # reload all the sensor network specifications
         pass  # to be updated
 
 
@@ -61,41 +62,56 @@ class SensorNode:
         """TO-DO: Να προσθέσω και ένα δεύτερο finger table για την επιστροφή του query"""
         self.node_data = "data"  # the sensor data stored on the node's memory
 
-    def update_finger_table(self):  # function to update the node's finger table
+    def update_finger_table(self):
+        # function to update the node's finger table
         pass  # to be updated
 
-    def update_successor_predecessor(self):  # function to update the successor and predecessor of the node
-        # TO-DO: ADD The cases for the first and the last nodes
-        pos = 0  # the position of this node on the chord ring
-        for i in range(0, self.network.List_of_Nodes):
-            if self.network.List_of_Nodes[i] < self.node_id:
-                pos += 1
-        self.successor_id = self.network.List_of_Nodes[pos+1]
-        self.predecessor_id = self.network.List_of_Nodes[pos-1]
+    def update_successor_predecessor(self):
+        # function to update the successor and predecessor of the node
+        if self.node_id == self.network.last_node:  # if the current node is the last node on the ring network
+            self.successor_id = self.network.List_of_Nodes[0]
+            self.predecessor_id = self.network.List_of_Nodes[self.network.Num_of_Nodes - 2]
+        elif self.node_id == self.network.first_node:  # if the current node is the first node on the ring network
+            self.successor_id = self.network.List_of_Nodes[1]
+            self.predecessor_id = self.network.List_of_Nodes[self.network.Num_of_Nodes - 1]
+        else:  # if the current node is any other node within the ring network
+            pos = 0  # the position of this node on the chord ring (zero value initially)
+            for i in range(0, self.network.Num_of_Nodes):  # find the position of the current node by incrementing pos
+                if self.network.List_of_Nodes[i] < self.node_id:
+                    pos += 1  # increment position
+            self.successor_id = self.network.List_of_Nodes[pos+1]  # successor is the next node
+            self.predecessor_id = self.network.List_of_Nodes[pos-1]  # predecessor is the previous
 
-    def find_successor(self, key):  # function to find the successor node to a lookup key
+    def find_successor(self, key):
+        # function to find the successor node to a lookup key
         pass  # to be updated
 
-    def find_closest_predecessor(self, key):  # function to find the closest predecessor node of a lookup key
+    def find_closest_predecessor(self, key):
+        # function to find the closest predecessor node of a lookup key
         pass  # to be updated
 
-    def node_join(self, ring_id):  # function to join the node on the chord ring network
+    def node_join(self, ring_id):
+        # function to join the node on the chord ring network
         pass  # to be updated
 
-    def node_leave(self):  # function to remove the node on the chord ring network
+    def node_leave(self):
+        # function to remove the node on the chord ring network
         pass  # to be updated
 
-    def lookup_query(self, value):  # function to look up a key
+    def lookup_query(self, value):
+        # function to look up a key
         pass  # to be updated
 
-    def sensor_data_reload(self):  # function to refresh the data from the node's sensors
+    def sensor_data_reload(self):
+        # function to refresh the data from the node's sensors
         pass  # to be updated
 
 
 """
 # main process
 """
-if __name__ == "__main__":  # Execute these lines, only if this module is executed by itself.
+if __name__ == "__main__":
+    # Execute these lines, only if this module is executed by itself.
     print("Chord Protocol Application on Wireless Sensor Networks")
     print("Implementation by Christos-Panagiotis Mpalatsouras")
     print("ORCID: orcid.org/0000-0001-8914-7559")
