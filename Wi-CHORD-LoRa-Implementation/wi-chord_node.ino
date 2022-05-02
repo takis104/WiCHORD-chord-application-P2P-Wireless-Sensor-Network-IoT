3b8e6041f7444d7c735dff10676cb29df6c8c35d
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2022-05-02 19:59:05 +0300

    Create wi-chord_node.ino
    Wi-CHORD Implementation for LoRa MCU Boards

---
+++ Wi-CHORD-LoRa-Implementation/wi-chord_node.ino
@@ -0,0 +1,15 @@
+/* Wi-CHORD: Chord Protocol application on a P2P LoRaWAN Wireless Sensor Network - IoT
+* Supported LoRa MCU: TTGO T-BEAM v1.1
+* Implemented by: Christos-Panagiotis Balatsouras
+* ORCID: https://orcid.org/0000-0001-8914-7559
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
