100046c234ef7c293603236f7b89e4948362217a
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2023-08-11 20:44:29 +0300

    Create SensorNodeMessage.py

---
+++ WiCHORD-Plus-Simulator-Python/SensorNodeMessage.py
@@ -0,0 +1,27 @@
+# Advanced Chord Implementation in Python
+# WiCHORD+ Wireless Sensor Network Overlay for LoRa WSNs on IoT Applications
+# Created by Christos-Panagiotis Balatsouras
+# ORCID: https://orcid.org/0000-0001-8914-7559
+
+"""
+Use Case: Application of Chord Protocol Based Routing in Wireless Sensor Networks
+Application Functionality:
+1. Node Join / Leave
+2. Network Build
+3. Data Lookup Query
+4. WiCHORD Evaluation on LoRa WSNs
+
+****
+FILE DESCRIPTION: SensorNode class definition
+"""
+
+class Message:
+    """
+    Class Message:
+    Data: Message Attributes
+    Operations:
+    To be added later...
+    """
+
+    def __int__(self, message_id):
+        pass
