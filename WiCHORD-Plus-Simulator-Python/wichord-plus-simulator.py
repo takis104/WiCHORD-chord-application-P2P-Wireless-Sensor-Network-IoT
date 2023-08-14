5363cca92c70061f4db8bf6be708181ee91a7d99
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2023-08-15 02:13:03 +0300

    Create wichord-plus-simulator.py

---
+++ WiCHORD-Plus-Simulator-Python/wichord-plus-simulator.py
@@ -0,0 +1,22 @@
+# Advanced Chord Implementation in Python
+# WiCHORD+ Wireless Sensor Network Overlay for LoRa WSNs on IoT Applications
+# Created by Christos-Panagiotis Balatsouras
+# ORCID: https://orcid.org/0000-0001-8914-7559
+
+"""
+Use Case: Application of Chord Protocol Based Routing in Wireless Sensor Networks
+Application Functionality:
+1. Node Join / Leave
+2. Network Build
+3. Data Lookup Query
+4. WiCHORD Evaluation on LoRa WSNs
+
+****
+FILE DESCRIPTION: WiCHORD+ Simulator in Python
+"""
+
+# import Python modules
+# ...
+
+# Import other modules from this project
+# ...
