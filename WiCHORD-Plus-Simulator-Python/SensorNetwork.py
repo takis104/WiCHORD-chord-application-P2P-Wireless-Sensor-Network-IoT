f4047a8bf6813dc2e24deeb87f71f7e3b05f5e89
Christos Balatsouras <52033650+takis104@users.noreply.github.com>
2023-08-14 04:21:06 +0300

    Update SensorNetwork.py

--- WiCHORD-Plus-Simulator-Python/SensorNetwork.py
+++ WiCHORD-Plus-Simulator-Python/SensorNetwork.py
@@ -1,4 +1,5 @@
 # Advanced Chord Implementation in Python
+# WiCHORD+ Wireless Sensor Network Overlay for LoRa WSNs on IoT Applications
 # Created by Christos-Panagiotis Balatsouras
 # ORCID: https://orcid.org/0000-0001-8914-7559
 
@@ -8,6 +9,7 @@ Application Functionality:
 1. Node Join / Leave
 2. Network Build
 3. Data Lookup Query
+4. WiCHORD Evaluation on LoRa WSNs
 
 ****
 FILE DESCRIPTION: SensorNetwork class definition
